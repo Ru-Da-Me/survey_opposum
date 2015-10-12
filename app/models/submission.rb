3b0cd2b702e333a4fdccfab9dfec829060e9730f
Ru-T <rwajnberg@gmail.com>
2015-10-12 15:24:48 -0400

    Do not allow a survey to be taken with answering all required questions

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -2,64 +2,13 @@ class Submission < ActiveRecord::Base
   belongs_to :survey
   has_many :answers
   has_many :questions, through: :answers
-  # validate :submit_require
+  validate :submit_require
   validates :survey_id, presence: true
   accepts_nested_attributes_for :answers
 
-  # def submit_require
-  #
-  #   survey.questions.each do |q|
-  #     if q.required
-  #       if q.answers.count > 0
-  #         q.answers.each do |a|
-  #           if a.answer_text.blank?
-  #             errors.add(:required, "blah blah")
-  #           end
-  #         end
-  #       else
-  #         errors.add(:required, "blah blah")
-  #       end
-  #     end
-  #   end
-
-  # def submit_require
-  #   survey.questions.each do |q|
-  #     if q.required
-  #       if q.answers.count > 0
-  #         q.answers.each do |a|
-  #           if a.answer_text.blank?
-  #             errors.add(:required, "ERROR: triggered by blank answer text")
-  #           end
-  #         end
-  #       # else
-  #       #   errors.add(:required, "ERROR: triggered by question answers not being > 0")
-  #       end
-  #     end
-  #   end
-  # end
-
-    # survey.questions.each do |q|
-    #   if q.required
-    #     q.answers.each do |a|
-    #       if a.answer_text.empty?
-    #         errors.add(:required, "Survey cannot be submitted without responding to required questions.")
-    #       end
-    #     end
-    #   end
-    # end
-    # if questions.any? { |q| q.required? } && (answers.any? { |a| a.answer_text.empty? || or  } || answers.count > 0)
-
-  #   answers.each do |a|
-  #     if a.answer_text.blank? && a.required
-  #       errors.add(:required, "Survey cannot be submitted without responding to #{question.text}.")
-  #     end
-  #   end
-
-  # validate :publish_with_require
-  #
-  # def publish_with_require
-  #   answers.any? { |a| a.answer_text.blank? && a.required }
-  #   errors.add(:required, "Survey cannot be submitted without completing all required questions")
-  # end
+  def submit_require
+    answers.any? { |a| a.answer_text.blank? && a.question.required }
+    errors.add(:required, "Survey cannot be submitted without completing all required questions")
+  end
 
 end
